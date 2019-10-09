736b12b0b6fd7e83c4e56b290652e63ad2940648
aoao <housinan@yuanli-inc.com>
2019-10-09 09:35:16 +0800

    register css

--- src/api/user.js
+++ src/api/user.js
@@ -22,3 +22,26 @@ export function logout() {
     method: 'post'
   })
 }
+
+export function register(data) {
+  return request({
+    url: '/auth/register',
+    method: 'post',
+    data
+  })
+}
+
+export function getCaptcha() {
+  return request({
+    url: '/misc/verify/verify',
+    method: 'get'
+  })
+}
+
+export function getMessageCode(data) {
+  return request({
+    url: 'misc/verify/sendSms',
+    method: 'post',
+    data
+  })
+}
