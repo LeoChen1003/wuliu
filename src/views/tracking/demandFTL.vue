fc5ab7cac96836e0041a04ae13002cce79698eb2
aoao <housinan@yuanli-inc.com>
2019-11-25 21:09:17 +0800

    fix demand跟单运输公司companyName/humanName二选一

--- src/views/tracking/demandFTL.vue
+++ src/views/tracking/demandFTL.vue
@@ -94,7 +94,7 @@
             <template slot-scope="scope">
               <div v-if="!(tabActive=='0'||tabActive=='1') && scope.row.transport.supply">
                 <div>
-                  {{ scope.row.transport.supply.companyName }}
+                  {{ scope.row.transport.supply.companyName == '' ? scope.row.transport.supply.humanName : scope.row.transport.supply.companyName }}
                 </div>
                 <div>
                   {{ scope.row.transport.supply.contactMobile }}
