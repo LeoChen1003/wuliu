2020b6aafbe82174c5c76c63d9eae29896f678a3
aoao <housinan@yuanli-inc.com>
2019-11-28 21:01:51 +0800

    save

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
+    path: "/login",
+    component: () => import("@/views/login/index"),
     hidden: true
   },
   {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/home/index'),
+    path: "/",
+    name: "home",
+    component: () => import("@/views/home/index"),
     hidden: true
   },
-  { path: '*', redirect: '/', hidden: true }
-]
+  { path: "*", redirect: "/", hidden: true }
+];
 
 /**
  * asyncRoutes
@@ -27,221 +27,226 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/booking',
+    path: "/booking",
     component: Layout,
-    redirect: '/booking',
-    name: 'booking',
+    redirect: "/booking",
+    name: "booking",
     meta: {
-      title: 'booking',
-      roles: ['DEMAND']
+      title: "booking",
+      roles: ["DEMAND"]
     },
     children: [
       {
-        path: 'priceConsulting',
-        component: () => import('@/views/booking/priceConsulting'),
-        name: 'priceConsulting',
-        meta: { title: 'priceConsulting', roles: ['DEMAND'] }
+        path: "priceConsulting",
+        component: () => import("@/views/booking/priceConsulting"),
+        name: "priceConsulting",
+        meta: { title: "priceConsulting", roles: ["DEMAND"] }
       },
       {
-        path: 'placeOrder',
-        component: () => import('@/views/booking/placeOrder'),
-        name: 'placeOrder',
+        path: "placeOrder",
+        component: () => import("@/views/booking/placeOrder"),
+        name: "placeOrder",
         hidden: true,
-        meta: { title: 'placeOrder', roles: ['DEMAND'] }
+        meta: { title: "placeOrder", roles: ["DEMAND"] }
       },
       {
-        path: 'releaseToMarket',
-        component: () => import('@/views/booking/releaseToMarket'),
-        name: 'releaseToMarket',
-        meta: { title: 'releaseToMarket', roles: ['DEMAND'] }
+        path: "releaseToMarket",
+        component: () => import("@/views/booking/releaseToMarket"),
+        name: "releaseToMarket",
+        meta: { title: "releaseToMarket", roles: ["DEMAND"] }
       }
     ]
   },
   {
-    path: '/market',
+    path: "/market",
     component: Layout,
-    redirect: '/market',
-    name: 'market',
+    redirect: "/market",
+    name: "market",
     meta: {
       // title: 'market',
-      roles: ['SUPPLY']
+      roles: ["SUPPLY"]
     },
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/market/index'),
-        meta: { title: 'market', roles: ['SUPPLY'] }
+        path: "index",
+        component: () => import("@/views/market/index"),
+        meta: { title: "market", roles: ["SUPPLY"] }
       }
     ]
   },
   {
-    path: '/tracking',
-    redirect: '/tracking',
+    path: "/tracking",
+    redirect: "/tracking",
     component: Layout,
-    name: 'tracking',
+    name: "tracking",
     meta: {
-      title: 'tracking',
-      roles: ['DEMAND', 'SUPPLY', 'PLATFORM']
+      title: "tracking",
+      roles: ["DEMAND", "SUPPLY", "PLATFORM"]
     },
     children: [
       {
-        path: 'demandFTL',
-        component: () => import('@/views/tracking/demandFTL'),
-        meta: { title: 'FTLNotHUB', roles: ['DEMAND'] }
+        path: "demandFTL",
+        component: () => import("@/views/tracking/demandFTL"),
+        meta: { title: "FTLNotHUB", roles: ["DEMAND"] }
       },
       {
-        path: 'FTL',
-        component: () => import('@/views/tracking/FTL'),
-        meta: { title: 'FTLNotHUB', roles: ['SUPPLY'] }
+        path: "FTL",
+        component: () => import("@/views/tracking/FTL"),
+        meta: { title: "FTLNotHUB", roles: ["SUPPLY"] }
       },
       {
-        path: 'platformFTL',
-        component: () => import('@/views/tracking/platformFTL'),
-        meta: { title: 'FTLNotHUB', roles: ['PLATFORM'] }
+        path: "platformFTL",
+        component: () => import("@/views/tracking/platformFTL"),
+        meta: { title: "FTLNotHUB", roles: ["PLATFORM"] }
       },
       {
-        path: 'LTL',
-        component: () => import('@/views/tracking/LTL'),
-        meta: { title: 'LTLHUB', roles: ['DEMAND', 'SUPPLY', 'PLATFORM'] }
+        path: "LTL",
+        component: () => import("@/views/tracking/LTL"),
+        meta: { title: "LTLHUB", roles: ["DEMAND", "SUPPLY", "PLATFORM"] }
       }
     ]
   },
   {
-    path: '/resources',
-    redirect: '/resources',
+    path: "/resources",
+    redirect: "/resources",
     component: Layout,
-    name: 'resources',
+    name: "resources",
     meta: {
-      title: 'resources',
-      roles: ['SUPPLY']
+      title: "resources",
+      roles: ["SUPPLY"]
     },
     children: [
       {
-        path: 'truck',
-        component: () => import('@/views/resources/truck'),
-        meta: { title: 'truck', roles: ['SUPPLY'] }
+        path: "truck",
+        component: () => import("@/views/resources/truck"),
+        meta: { title: "truck", roles: ["SUPPLY"] }
       },
       {
-        path: 'driver',
-        component: () => import('@/views/resources/driver'),
-        meta: { title: 'driver', roles: ['SUPPLY'] }
+        path: "driver",
+        component: () => import("@/views/resources/driver"),
+        meta: { title: "driver", roles: ["SUPPLY"] }
       },
       {
-        path: 'routeFTL',
-        component: () => import('@/views/resources/routeFTL'),
-        meta: { title: 'routeFTL', roles: ['SUPPLY'] }
+        path: "routeFTL",
+        component: () => import("@/views/resources/routeFTL"),
+        meta: { title: "routeFTL", roles: ["SUPPLY"] }
       },
       {
-        path: 'routeLTL',
-        component: () => import('@/views/resources/routeLTL'),
-        meta: { title: 'routeLTL', roles: ['SUPPLY'] }
+        path: "routeLTL",
+        component: () => import("@/views/resources/routeLTL"),
+        meta: { title: "routeLTL", roles: ["SUPPLY"] }
       },
       {
-        path: 'planningLTL',
-        component: () => import('@/views/resources/planningLTL'),
-        meta: { title: 'planningLTL', roles: ['SUPPLY'] }
+        path: "planningLTL",
+        component: () => import("@/views/resources/planningLTL"),
+        meta: { title: "planningLTL", roles: ["SUPPLY"] }
       }
     ]
   },
   {
-    path: '/billing',
-    redirect: '/billing',
+    path: "/billing",
+    redirect: "/billing",
     component: Layout,
-    name: 'billing',
+    name: "billing",
     meta: {
-      title: 'billing',
-      roles: ['DEMAND', 'SUPPLY', 'HUB', 'PLATFORM']
+      title: "billing",
+      roles: ["DEMAND", "SUPPLY", "HUB", "PLATFORM"]
     },
     children: [
       {
-        path: 'payableBill',
-        component: () => import('@/views/billing/payableBill'),
-        meta: { title: 'payableBill', roles: ['DEMAND'] }
+        path: "payableBill",
+        component: () => import("@/views/billing/payableBill"),
+        meta: { title: "payableBill", roles: ["DEMAND"] }
       },
       {
-        path: 'receivable',
-        component: () => import('@/views/billing/receivable'),
-        meta: { title: 'receivable', roles: ['SUPPLY'] }
+        path: "receivable",
+        component: () => import("@/views/billing/receivable"),
+        meta: { title: "receivable", roles: ["SUPPLY"] }
       },
       {
-        path: 'topUp',
-        component: () => import('@/views/billing/topUp'),
-        meta: { title: 'topUp', roles: ['DEMAND', 'SUPPLY', 'HUB'] }
+        path: "topUp",
+        component: () => import("@/views/billing/topUp"),
+        meta: { title: "topUp", roles: ["DEMAND", "SUPPLY", "HUB"] }
       },
       {
-        path: 'journal',
-        component: () => import('@/views/billing/journal'),
-        meta: { title: 'journal', roles: ['DEMAND', 'SUPPLY', 'HUB'] }
+        path: "journal",
+        component: () => import("@/views/billing/journal"),
+        meta: { title: "journal", roles: ["DEMAND", "SUPPLY", "HUB"] }
       },
       {
-        path: 'platform',
-        component: () => import('@/views/billing/platform'),
-        meta: { title: 'billing', roles: ['PLATFORM'] }
+        path: "platform",
+        component: () => import("@/views/billing/platform"),
+        meta: { title: "billing", roles: ["PLATFORM"] }
+      },
+      {
+        path: "transferFreight",
+        component: () => import("@/views/billing/transferFreight"),
+        meta: { title: "transferFreight", roles: ["PLATFORM"] }
       }
     ]
   },
   {
-    path: '/member',
-    redirect: '/member',
+    path: "/member",
+    redirect: "/member",
     component: Layout,
-    name: 'member',
+    name: "member",
     meta: {
-      title: 'member',
-      roles: ['DEMAND', 'SUPPLY', 'HUB', 'PLATFORM']
+      title: "member",
+      roles: ["DEMAND", "SUPPLY", "HUB", "PLATFORM"]
     },
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/member/index'),
-        meta: { title: 'member', roles: ['DEMAND', 'SUPPLY', 'HUB'] }
+        path: "index",
+        component: () => import("@/views/member/index"),
+        meta: { title: "member", roles: ["DEMAND", "SUPPLY", "HUB"] }
       },
       {
-        path: 'toBeVerified',
-        component: () => import('@/views/member/toBeVerified'),
-        meta: { title: 'toBeVerified', roles: ['PLATFORM'] }
+        path: "toBeVerified",
+        component: () => import("@/views/member/toBeVerified"),
+        meta: { title: "toBeVerified", roles: ["PLATFORM"] }
       },
       {
-        path: 'rejected',
-        component: () => import('@/views/member/rejected'),
-        meta: { title: 'rejected', roles: ['PLATFORM'] }
+        path: "rejected",
+        component: () => import("@/views/member/rejected"),
+        meta: { title: "rejected", roles: ["PLATFORM"] }
       },
       {
-        path: 'toBeSignedContract',
-        component: () => import('@/views/member/toBeSignedContract'),
-        meta: { title: 'toBeSignedContract', roles: ['PLATFORM'] }
+        path: "toBeSignedContract",
+        component: () => import("@/views/member/toBeSignedContract"),
+        meta: { title: "toBeSignedContract", roles: ["PLATFORM"] }
       },
       {
-        path: 'activated',
-        component: () => import('@/views/member/activated'),
-        meta: { title: 'activated', roles: ['PLATFORM'] }
+        path: "activated",
+        component: () => import("@/views/member/activated"),
+        meta: { title: "activated", roles: ["PLATFORM"] }
       },
       {
-        path: 'closed',
-        component: () => import('@/views/member/closed'),
-        meta: { title: 'closed', roles: ['PLATFORM'] }
+        path: "closed",
+        component: () => import("@/views/member/closed"),
+        meta: { title: "closed", roles: ["PLATFORM"] }
       }
     ]
   }
-]
+];
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
     routes: constantRoutes
-  })
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
 router.$addRoutes = params => {
-  router.matcher = new Router().matcher
-  router.addRoutes(params)
-}
+  router.matcher = new Router().matcher;
+  router.addRoutes(params);
+};
 
-export default router
+export default router;
---
+++ src/views/billing/transferFreight.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="manage billing">
+    <div class="statusHeader">
+      <div class="status-txt">{{ $t('billing.billingStatus') }}</div>
+      <div class="timePicker">
+        <bcTime @changeBCtime="changeBCtimeFrom"
+                :timeType="'all'"
+                :dateDefault='fromDateDeFault'></bcTime>
+        <span style="margin:0 5px;">至</span>
+        <bcTime @changeBCtime="changeBCtimeTo"
+                :dateDefault='toDateDeFault'
+                style="margin-left:5px;"
+                :timeType="'all'"></bcTime>
+        <el-button size="small"
+                   @click="searchIt"
+                   style='width:100px;margin-left:20px;'>{{ $t('billing.search') }}</el-button>
+      </div>
+    </div>
+    <div class="content">
+      <div>
+        <el-tabs v-model="tabActive"
+                 tab-position="left"
+                 @tab-click="handleClick"
+                 style="height:calc(100% - 50px);">
+          <el-tab-pane name="WAIT_SETTLE">
+            <span slot="label">
+              <div class="tabLabel">
+                <div class="text">{{$t('billing.unpaid_TF')}}<sub class="badge red">{{statusCount.WAIT_SETTLE}}</sub></div>
+              </div>
+            </span>
+          </el-tab-pane>
+          <el-tab-pane name="SETTLED">
+            <span slot="label">
+              <div class="tabLabel">
+                <div class="text">{{$t('billing.paid_TF')}}<sub class="badge">{{statusCount.SETTLED}}</sub></div>
+              </div>
+            </span>
+          </el-tab-pane>
+          <el-tab-pane name="CANCELED">
+            <span slot="label">
+              <div class="tabLabel">
+                <div class="text">{{$t('billing.canceled_TF')}}<sub class="badge">{{statusCount.CANCELED}}</sub></div>
+              </div>
+            </span>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+      <div class="container">
+        <div class="center">
+          <el-table :data="tableData"
+                    highlight-current-row
+                    v-loading="loading"
+                    @current-change="handleCurrentChange"
+                    border>
+            <el-table-column prop="orderNo"
+                             :label="$t('billing.trackingNo')" />
+            <el-table-column prop="createdAt"
+                             :label="$t('billing.supply')" />
+            <el-table-column prop="settlementAmount"
+                             :label="$t('billing.totalAmount')" />
+            <el-table-column align="right">
+              <template slot="header"
+                        slot-scope="scope">
+                <el-input v-model="search"
+                          size="mini"
+                          placeholder="输入关键字搜索" />
+              </template>
+              <template slot-scope="scope">
+                <el-checkbox ></el-checkbox>
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-pagination style="margin-top:10px;text-align: center;margin-bottom:50px;"
+                         background
+                         :page-sizes="[1,5,10,20,50]"
+                         :page-size="pagesize"
+                         @size-change="pageSizeChange"
+                         :current-page.sync="page.currentPage"
+                         @current-change="pageChange"
+                         layout="prev, pager, next, jumper"
+                         :total="page.total"></el-pagination>
+        </div>
+        <div class="right">
+          <el-table :data="detailData"
+                    border>
+            <el-table-column :label="$t('billing.supply')">
+              <template slot-scope="scope">
+                {{scope.row.transport.supply?scope.row.transport.supply.companyName:''}}
+              </template>
+            </el-table-column>
+            <el-table-column :label="$t('billing.amount')">
+              <template slot-scope="scope">
+                {{$t('billing.freight')}}: {{scope.row.settlementAmount}}
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { supplyFinance, billsupplyCount } from '../../api/billing'
+import { getTime, parseTime, getLastMonthTime } from '../../utils/index'
+import bcTime from "@/components/bcTime";
+
+let self
+export default {
+  // import引入的组件需要注入到对象中才能使用
+  components: { bcTime },
+  data () {
+    return {
+      tabActive: 'WAIT_SETTLE',
+      fromDate: getLastMonthTime(new Date()),
+      toDate: parseTime(new Date().getTime(), '{y}-{m}-{d}'),
+      tableData: [],
+      page: {
+        total: 0,
+        currentPage: 1
+      },
+      pagesize: 20,
+      loading: false,
+      detailData: [],
+      statusCount: {}
+    };
+  },
+  // 监听属性 类似于data概念
+  computed: {
+    fromDateDeFault () {
+      return self.fromDate.split('-')
+    },
+    toDateDeFault () {
+      return self.toDate.split('-')
+    },
+  },
+  // 监控data中的数据变化
+  watch: {},
+  created () {
+    self = this
+  },
+  mounted () { },
+  methods: {
+    changeBCtimeFrom (time) {
+      self.fromDate = time
+    },
+    changeBCtimeTo (time) {
+      self.toDate = time
+    },
+    pageChange (val) {
+      let self = this
+      self.page.currentPage = val
+      self.loadData()
+    },
+    pageSizeChange (val) {
+      let self = this;
+      self.pagesize = val
+      self.loadData()
+    },
+    loadData () {
+      self.loading = true
+      supplyFinance(self.tabActive, {
+        start: self.fromDate + ' 00:00:00',
+        end: self.toDate + ' 23:59:59',
+        page: self.page.currentPage - 1,
+        pagesize: self.pagesize
+      }).then(res => {
+        self.tableData = res.data.content
+        self.page = {
+          total: res.data.totalElements,
+          currentPage: res.data.number + 1
+        }
+        self.loading = false
+      }).catch(() => {
+        self.loading = false
+      })
+      billsupplyCount({
+        start: self.fromDate + ' 00:00:00',
+        end: self.toDate + ' 23:59:59',
+      }).then(res => {
+        self.statusCount = res.data
+      })
+    },
+    searchIt () {
+      self.loadData()
+    },
+    handleClick () {
+      self.loadData()
+    },
+    handleCurrentChange (val) {
+      const self = this
+      self.detailData = []
+      self.detailData.push(val)
+    }
+  }
+};
+</script>
+<style lang='scss' scoped>
+.manage {
+  height: 100%;
+  box-sizing: border-box;
+  .statusHeader {
+    display: flex;
+    padding: 0px 20px;
+    box-sizing: border-box;
+    height: 50px;
+    border-bottom: 2px solid #dfe4ed;
+    align-items: center;
+    .status-txt {
+      height: 50px;
+      line-height: 50px;
+      padding-left: 20px;
+      font-size: 20px;
+      width: 216px;
+      box-sizing: border-box;
+      border-right: 2px solid #dfe4ed;
+    }
+  }
+  .timePicker {
+    height: 42px;
+    line-height: 40px;
+    padding-left: 30px;
+    display: flex;
+    align-items: center;
+  }
+  .content {
+    padding-left: 25px;
+    display: flex;
+    height: calc(100% - 50px);
+    .container {
+      padding-left: 20px;
+      padding-top: 20px;
+      width: 100%;
+      display: flex;
+      overflow: scroll;
+      .center {
+        width: 49%;
+        margin-right: 1%;
+      }
+      .right {
+        width: 49%;
+      }
+    }
+  }
+}
+.tabLabel {
+  display: flex;
+  justify-content: flex-end;
+
+  .badge {
+    font-size: 12px;
+    margin-left: 5px;
+    color: #aaa;
+  }
+
+  .red {
+    color: red;
+  }
+}
+</style>
+<style>
+.billing .el-tabs--left .el-tabs__header.is-left {
+  margin-right: 0px;
+  width: 211px;
+}
+.billing .el-tabs--left .el-tabs__active-bar.is-left {
+  width: 3px;
+}
+.billing .el-tabs--left .el-tabs__nav-wrap.is-left::after,
+.el-tabs--left .el-tabs__nav-wrap.is-right::after,
+.el-tabs--right .el-tabs__nav-wrap.is-left::after,
+.el-tabs--right .el-tabs__nav-wrap.is-right::after {
+  width: 3px;
+}
+</style>
+
+
