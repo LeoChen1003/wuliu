3dbbe59cf1e5f825fc4a4a7c1fbd093feb4e6905
aoao <housinan@yuanli-inc.com>
2019-12-09 14:32:59 +0800

    fix home.vue

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,7 +6,8 @@
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 import store from "@/store";
-import { resetRouter, router } from "@/router";
+import router from '@/router';
+import { resetRouter } from "@/router";
 
 import { mapGetters } from "vuex";
 
