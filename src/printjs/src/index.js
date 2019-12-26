b919e9154818d712a25fe76fec3c66d4bce5ff50
aoao <housinan@yuanli-inc.com>
2019-12-26 09:39:44 +0800

    T 测试WL环境打印

--- src/printjs/src/index.js
+++ src/printjs/src/index.js
@@ -1,10 +1,10 @@
-import './sass/index.scss'
-import print from './js/init'
+import "./sass/index.scss";
+import print from "./js/init";
 
-const printJS = print.init
+const printJS = print.init;
 
-if (typeof window !== 'undefined') {
-  window.printJS = printJS
+if (typeof window !== "undefined") {
+  window.printJS = printJS;
 }
 
-export default printJS
+export default printJS;
