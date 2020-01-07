2b40d94360f785339db870215d748f4433338b77
aoao <housinan@yuanli-inc.com>
2020-01-07 14:16:31 +0800

    F try fix

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
     disableHostCheck: true,
     overlay: {
       warnings: false,
-      errors: true
+      errors: true,
     },
     proxy: {
       // change xxx-api/login => mock/login
@@ -41,11 +41,11 @@ module.exports = {
         target: `http://127.0.0.1:${port}/mock`,
         changeOrigin: true,
         pathRewrite: {
-          ["^" + process.env.VUE_APP_BASE_API]: ""
-        }
-      }
+          ["^" + process.env.VUE_APP_BASE_API]: "",
+        },
+      },
     },
-    after: require("./mock/mock-server.js")
+    after: require("./mock/mock-server.js"),
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -60,12 +60,12 @@ module.exports = {
         "@comp": resolve("src/components"),
         "@api": resolve("src/api"),
         "@router": resolve("src/router"),
-        "@utils": resolve("src/utils")
-      }
+        "@utils": resolve("src/utils"),
+      },
     },
     externals: {
-      google: "google"
-    }
+      google: "google",
+    },
   },
   chainWebpack(config) {
     config.plugins.delete("preload"); // TODO: need test
@@ -84,7 +84,7 @@ module.exports = {
       .use("svg-sprite-loader")
       .loader("svg-sprite-loader")
       .options({
-        symbolId: "icon-[name]"
+        symbolId: "icon-[name]",
       })
       .end();
 
@@ -101,9 +101,7 @@ module.exports = {
 
     config
       // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === "development", config =>
-        config.devtool("cheap-source-map")
-      );
+      .when(process.env.NODE_ENV === "development", config => config.devtool("cheap-source-map"));
 
     config.when(process.env.NODE_ENV !== "development", config => {
       config
@@ -112,8 +110,8 @@ module.exports = {
         .use("script-ext-html-webpack-plugin", [
           {
             // `runtime` must same as runtimeChunk name. default is `runtime`
-            inline: /runtime\..*\.js$/
-          }
+            inline: /runtime\..*\.js$/,
+          },
         ])
         .end();
       config.optimization.splitChunks({
@@ -123,23 +121,23 @@ module.exports = {
             name: "chunk-libs",
             test: /[\\/]node_modules[\\/]/,
             priority: 10,
-            chunks: "initial" // only package third parties that are initially dependent
+            chunks: "initial", // only package third parties that are initially dependent
           },
           elementUI: {
             name: "chunk-elementUI", // split elementUI into a single package
             priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
           },
           commons: {
             name: "chunk-commons",
             test: resolve("src/components"), // can customize your rules
             minChunks: 3, //  minimum common number
             priority: 5,
-            reuseExistingChunk: true
-          }
-        }
+            reuseExistingChunk: true,
+          },
+        },
       });
       config.optimization.runtimeChunk("single");
     });
-  }
+  },
 };
